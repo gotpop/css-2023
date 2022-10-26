5b03bea9b424aaf066a79452634ccf67154e2120
gotpop <liam@gotpop.net>
2022-10-26 15:57:43 +0100

    Templating

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,5 +1,10 @@
-import FooterImport from './Footer'
+import Link from 'next/link'
+import styles from './Footer.module.css'
 
-const Footer =  FooterImport
-
-export default Footer
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div>Copyright © {new Date().getFullYear()}. All rights reserved.</div>
+    </footer>
+  )
+}
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,5 +1,18 @@
-import HeaderImport from './Header'
+import Link from 'next/link'
+import * as React from 'react'
+import styles from './Header.module.css'
 
-const Header =  HeaderImport
+export default function NavBar({ content }) {
+  return (
+    <header className={styles.header}>
+      <h1>Header</h1>
 
-export default Header
+      <a className={styles.nav} href="https://github.com/gotpop/ui-system">
+        GitHub
+      </a>
+      <a className={styles.nav} href="http://localhost:3000">
+        LocalHost
+      </a>
+    </header>
+  )
+}
