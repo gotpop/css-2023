6fe2338255e2fcd15a251dadb2f280122bd67d81
gotpop <liam@gotpop.net>
2022-10-26 15:14:21 +0100

    Templating

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,19 @@
 import { useState } from 'react'
 import Link from 'next/link'
 import type { NextPage } from 'next'
+
 import Layout from '@layouts/layout'
 import Intro from '@components/Intro'
-import { cards } from '@content/cards'
 import Card from '@components/Card'
+import { cards } from '@content/cards'
 
 export default function Home() {
-  const [bookmarksChecked, setBookmarksChecked] = useState(true)
-  const [urlsChecked, setUrlsChecked] = useState(false)
-  const [person, setPerson] = useState('pedro')
-
   return (
     <Layout>
       {/* <Intro content={introContent} /> */}
-      {/* <h1>Radix UI</h1> */}
-      {cards.map(card => <Card key={card.title} content={card} />)}
+      {cards.map(card => (
+        <Card key={card.title} content={card} />
+      ))}
     </Layout>
   )
 }
