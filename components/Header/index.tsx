88e37830666056007801763e8a66c4a2f7e6b176
gotpop <liam@gotpop.net>
2022-10-27 16:34:51 +0100

    Templating

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link'
 import * as React from 'react'
 import styles from './Header.module.css'
 
 export default function NavBar({ content }) {
   return (
     <header className={styles.header}>
-      <h1>Header</h1>
+      <Link href="/">
+        <h1>Header</h1>
+      </Link>
       <a className={styles.nav} href="https://github.com/gotpop/ui-system">
         GitHub
       </a>
