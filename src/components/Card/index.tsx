d75e7a9002d48b5644f69503fa515e0fae12f27c
gotpop <liam@gotpop.net>
2022-10-28 21:12:22 +0100

    Adding react icons

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import styles from './Card.module.css'
+import { TfiGithub } from "react-icons/tfi";
+import { SiCsswizardry, SiMozilla } from "react-icons/si";
 
 export default function Card({ content }) {
   const { title, text } = content
@@ -8,6 +10,9 @@ export default function Card({ content }) {
     <article className={styles.card}>
       <h1>{title}</h1>
       <p>{text}</p>
+      <TfiGithub />
+      <SiCsswizardry />
+      <SiMozilla />
     </article>
   )
 }
