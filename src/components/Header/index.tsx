9911797035067c6d3042c787f0cc126d0b4311b5
gotpop <liam@gotpop.net>
2022-10-30 03:18:21 +0000

    Nav buttons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,29 @@
 import Link from 'next/link'
 import * as React from 'react'
+import { TfiGithub } from 'react-icons/tfi'
+import { GoZap } from 'react-icons/go'
+import { AiFillHome } from 'react-icons/ai'
 import styles from './Header.module.css'
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <Link className='logo' href="/">
-        <h1 >CSS 2023</h1>
+      <Link className="logo" href="/">
+        <h1>CSS 2023</h1>
       </Link>
       <nav className={styles.nav}>
-        <a href="https://github.com/gotpop/ui-system">GitHub</a>
-        <a href="http://localhost:3000">LocalHost</a>
-        <a href="https://ui-system-gold.vercel.app/">Live</a>
+        <a href="https://github.com/gotpop/ui-system">
+          <span>GitHub</span>
+          <AiFillHome />
+        </a>
+        <a href="http://localhost:3000">
+          <span>LocalHost</span>
+          <TfiGithub />
+        </a>
+        <a href="https://ui-system-gold.vercel.app/">
+          <span>Live</span>
+          <GoZap />
+        </a>
       </nav>
     </header>
   )
