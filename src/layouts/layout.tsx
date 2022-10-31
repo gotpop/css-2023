ed4e3d6301c8fbb13fdc2b2f710742db5b1ec390
gotpop <liam@gotpop.net>
2022-10-31 20:40:46 +0000

    Scrollbar width

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,20 +1,22 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-// import MenuContext from '@context/MenuContext'
 import Trigger from '@components/Trigger'
 import styles from './Site.module.css'
 
+const getScrollBarWidth = () => {
+  let root = document.documentElement
+  const width = window.innerWidth - root.clientWidth
+
+  root.style.setProperty('--scrollbar-width', `${width}px`)
+}
+
 export default function Layout(props) {
   const { children } = props
-  // const { menu } = useContext(MenuContext)
 
   useEffect(() => {
-    document.documentElement.style.setProperty(
-      '--scrollbar-width',
-      window.innerWidth - document.documentElement.clientWidth + 'px'
-    )
+    getScrollBarWidth()
   }, [])
 
   return (
@@ -22,6 +24,7 @@ export default function Layout(props) {
       <Head>
         <title>CSS 2023</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, user-scalable=no" />
       </Head>
       <div className={styles.site}>
         <Header />
